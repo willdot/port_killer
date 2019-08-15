6def5f76375ffedc8fb3ce11ed45c5d6058302cb
Will <will7989@hotmail.com>
2019-08-15 10:41:30 +0100

    refactor

--- main.go
+++ main.go
@@ -9,34 +9,46 @@ import (
 	"strings"
 )
 
+var err error
+
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
+
 	fmt.Print("Enter port number you wish to kill:")
-	port, _ := reader.ReadString('\n')
 
-	port = ":" + strings.TrimRight(port, " \n")
-
-	args := []string{
-		"-i", port,
-	}
-
-	output, err := exec.Command("lsof", args...).CombinedOutput()
+	port, err := readFromUserAndTrim(reader)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	result := string(output)
-	fmt.Println("\n" + result)
-	pid := strings.Fields(result)[10]
+	port = ":" + port
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	pid, err := getPid(port)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	fmt.Printf("Are you sure you wish to kill process with PID: %v? Y or N\n", pid)
 
-	confirm, _ := reader.ReadString('\n')
+	confirm, err := readFromUserAndTrim(reader)
 
-	if strings.TrimRight(confirm, " \n") != "Y" {
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if confirm != "Y" {
+		fmt.Println("Exiting without killing port")
 		return
 	}
 
@@ -45,14 +57,49 @@ func main() {
 		return
 	}
 
-	args = []string{
-		pid,
-	}
-
-	_, err = exec.Command("kill", args...).CombinedOutput()
+	_, err = killProcess(pid)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+}
+
+func killProcess(pid string) ([]byte, error) {
+	args := []string{
+		pid,
+	}
+
+	return exec.Command("kill", args...).CombinedOutput()
+}
+
+func getPid(port string) (string, error) {
+	args := []string{
+		"-i", port,
+	}
+
+	procssDetails, err := exec.Command("lsof", args...).CombinedOutput()
+
+	if err != nil {
+		return "", err
+	}
+
+	processString := string(procssDetails)
+	// Print the process details to the user so they can see what the result is before they confirm the kill
+	fmt.Println("\n" + processString)
+
+	return strings.Fields(processString)[10], nil
+}
+
+func readFromUserAndTrim(reader *bufio.Reader) (string, error) {
+
+	result, err := reader.ReadString('\n')
+
+	if err != nil {
+		return "", err
+	}
+
+	result = strings.TrimRight(result, "\n")
+
+	return result, nil
 }
